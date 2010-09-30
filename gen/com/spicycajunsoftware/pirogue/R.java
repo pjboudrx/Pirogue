189a43cb736d664b1ca4392a6587c651d68b101c
Patrick Boudreaux <patrick.boudreaux@versionone.com>
2010-09-29 22:22:29 -0400

    don't invoke car mode on launch; added menu w/ exit button

--- gen/com/spicycajunsoftware/pirogue/R.java
+++ gen/com/spicycajunsoftware/pirogue/R.java
@@ -11,7 +11,8 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int icon=0x7f020000;
+        public static final int exit=0x7f020000;
+        public static final int icon=0x7f020001;
     }
     public static final class layout {
         public static final int main=0x7f030000;
@@ -19,5 +20,6 @@ public final class R {
     public static final class string {
         public static final int app_name=0x7f040001;
         public static final int hello=0x7f040000;
+        public static final int menu_exit=0x7f040002;
     }
 }
--- src/com/spicycajunsoftware/pirogue/PirogueHome.java
+++ src/com/spicycajunsoftware/pirogue/PirogueHome.java
@@ -4,15 +4,35 @@ import android.app.Activity;
 import android.app.UiModeManager;
 import android.content.Context;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class PirogueHome extends Activity {
+	private static final int Exit_ID = Menu.FIRST;
+	
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+    }
+    
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        MenuItem exit = menu.add(0, Exit_ID,0, R.string.menu_exit);
+        exit.setIcon(R.drawable.exit);
+        return true;
+    }
+
+    @Override
+    public boolean onMenuItemSelected(int featureId, MenuItem item) {
+        switch(item.getItemId()) {
+        case Exit_ID:
+            finish();
+            return true;
+        }
         
-//        UiModeManager modeManager = (UiModeManager) getSystemService(Context.UI_MODE_SERVICE);
-//        modeManager.enableCarMode(0);
+        return super.onMenuItemSelected(featureId, item);
     }
 }
